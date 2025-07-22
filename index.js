86f00299a60df3a657dd7489f48adbcb4ce66d03
Muhsin-Gun <muhsinabdi288@gmail.com>
2025-07-22 11:51:53 +0300

    mongoose connected

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// const authRoutes = require('./routes/authRoutes');
+// const eventRoutes = require('./routes/eventRoutes');
+// const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/events', eventRoutes);
+// app.use('/api/users', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: process.env.DB_NAME
+}).then(() => {
+  console.log("✅ MongoDB connected");
+  app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+  });
+}).catch(err => console.error("❌ Mongo error:", err.message));
